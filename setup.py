52798d216e72249bb494d7101203d8d48c4524ff
Paddy Foran <paddy@secondbit.org>
2012-02-02 16:59:45 -0500

    Updated for pypi install.

--- setup.py
+++ setup.py
@@ -11,11 +11,15 @@ required = ["poster"]
 
 
 setup(
-    name='iron_worker',
-    version='0.0.1',
-    description='A Python client for iron.io, the leader in cloud queueing systems.',
-    author='iron.io',
-    url='http://iron.io',
+    name='iron-worker',
+    version='0.0.7',
+    description='The Python client for IronWorker, a cloud service for background processing.',
+    long_description='IronWorker is a background processing and task queuing system that lets your applications use the cloud to do their heavy lifting. This is the Python library to interface with the API.',
+    keywords=['iron', 'ironio', 'iron.io', 'iron-io', 'ironworker', 'iron-worker', 'iron_worker', 'worker', 'cloud', 'task queue', 'background processing'],
+    author='Iron.io',
+    author_email="support@iron.io",
+    url='http://iron.io/products/worker',
     install_requires=required,
-    packages=[ 'iron_worker' ],
+    packages=[ '' ],
+    package_dir={'':'iron_worker'},
 )
