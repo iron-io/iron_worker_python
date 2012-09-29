271594af2eacf81c0649b107130bcb74a4f5937f
Paddy Foran <foran.paddy@gmail.com>
2012-09-29 17:21:08 -0400

    Incremented library version for PyPi.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ required = ["poster"]
 
 setup(
     name='iron-worker',
-    version='0.0.12.1',
+    version='0.0.13',
     description='The Python client for IronWorker, a cloud service for background processing.',
     long_description='IronWorker is a background processing and task queuing system that lets your applications use the cloud to do their heavy lifting. This is the Python library to interface with the API.',
     keywords=['iron', 'ironio', 'iron.io', 'iron-io', 'ironworker', 'iron-worker', 'iron_worker', 'worker', 'cloud', 'task queue', 'background processing'],
