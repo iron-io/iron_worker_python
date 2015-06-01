d0f52dd19b148e484f0960869522d159df81c86f
Alex <alex.litvak@iron.io>
2015-05-31 23:59:10 -0700

    Update version

--- iron_worker.py
+++ iron_worker.py
@@ -238,7 +238,7 @@ class CodePackage:
 
 class IronWorker:
     NAME = "iron_worker_python"
-    VERSION = "1.3.0"
+    VERSION = "1.3.3"
     
     isLoaded = False
     arguments = {'task_id': None, 'dir': None, 'payload': None, 'config': None}
