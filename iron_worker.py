1cd5d17d19b9877ff825d713612f5212e3d06b0d
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2015-08-04 10:41:00 +0600

    Bump version to 1.3.5

--- iron_worker.py
+++ iron_worker.py
@@ -238,7 +238,7 @@ class CodePackage:
 
 class IronWorker:
     NAME = "iron_worker_python"
-    VERSION = "1.3.4"
+    VERSION = "1.3.5"
     
     isLoaded = False
     arguments = {'task_id': None, 'dir': None, 'payload': None, 'config': None}
